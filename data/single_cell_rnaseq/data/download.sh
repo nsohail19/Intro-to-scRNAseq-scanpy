f1f6ea92b56b13b7cb559f0d46c34a432e4a2fcd
nsohail19 <20noor.sohail@gmail.com>
2025-08-03 17:38:51 -0400

    Download count matrix files

--- data/single_cell_rnaseq/data/download.sh
+++ data/single_cell_rnaseq/data/download.sh
@@ -3,3 +3,4 @@
 data_url="https://www.dropbox.com/scl/fo/zy86o0a3zfza7g1vnaw4x/AFCsUwI0u1sPWuJ7FoYmenc?rlkey=qplebwrtbm4s4mmktg0oyf9xi&st=6eea9989&dl=1"
 
 curl -L "$data_url" -o data.zip
+unzip data.zip
